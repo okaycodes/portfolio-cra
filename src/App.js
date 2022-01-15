21843c52e19c17b10275c246eea47ffe02b90c63
okaycodes <fo.okenana@gmail.com>
2022-01-15 05:34:36 +0100

    updated and rendered AboutMe section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import HeaderContainer from "./containers/header";
+import HomeContainer from "./containers/home";
+import AboutMeContainer from "./containers/about";
+import {COLORS, FONTS} from './constants/styles';
+import {ThemeProvider} from "styled-components";
 
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={{colors: COLORS, fonts: FONTS}}>
       <HeaderContainer/>
-    </div>
+      <HomeContainer/>
+      <AboutMeContainer/>
+    </ThemeProvider>
   );
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,3 @@
 export {default as Header} from "./header"
+export {default as Home} from "./home"
+export {default as AboutMe} from "./about"
