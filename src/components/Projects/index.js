70d6d3fdd7f44b54d0840f32c61b713230289539
okaycodes <fo.okenana@gmail.com>
2022-02-04 22:10:00 +0100

    created projects component

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -5,6 +5,10 @@ export default function Projects({children, ...restProps}){
     return <Container {...restProps}><Inner>{children}</Inner></Container>
 }
 
-Projects.Title = function ProjectTitle({children, ...restProps}){
+Projects.Title = function ProjectsTitle({children, ...restProps}){
     return <Title {...restProps}>{children}</Title>
+}
+
+Projects.Project = function Project({children, ...restProps}){
+    return <Project {...restProps}>{children}</Project>
 }
--- src/components/Projects/styles/projects.js
+++ src/components/Projects/styles/projects.js
@@ -6,13 +6,33 @@ export const Container = styled.section`
 `;
 
 export const Inner = styled.div`
+
 `;
 
 export const Title = styled.h2`
 
 `;
 
+export const ProjectsList = styled.div`
+
+`;
 
 export const Project = styled.div`
 
 `
+
+export const ProjectTitle = styled.h3`
+
+`;
+
+export const ProjectImage = styled.img`
+
+`;
+
+export const ProjectText = styled.p`
+
+`;
+
+export const ProjectLink = styled.a`
+
+`;
