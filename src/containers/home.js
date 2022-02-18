afd5824899cd23afa0f29c64cbe2743e8690b6ee
okaycodes <fo.okenana@gmail.com>
2022-02-18 20:26:03 +0100

    minor changes

--- src/containers/home.js
+++ src/containers/home.js
@@ -4,7 +4,7 @@ import {Home} from './../components'
 export default function HomeContainer(){
     return (
         <Home>
-            <Home.Title>Hi, there My name is Okay codes <br/> Welcome to my <br/> Frontend Portfolio</Home.Title>
+            <Home.Title>Hi, there My name is Okenana Favour <br/> Welcome to my <br/> Frontend Portfolio</Home.Title>
         </Home>
     )
 }
