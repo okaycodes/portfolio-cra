dbd6d50e75baec64f6802127f0486b2041d7c9c6
okaycodes <fo.okenana@gmail.com>
2023-01-10 22:46:08 +0100

    create dev branch

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,24 +1,27 @@
-import {Header} from './../components'
-import {useState} from 'react'
+import { Header } from "./../components";
+import { useState } from "react";
 
-
-export default function HeaderContainer (){
-    const navArray = ['HOME', 'ABOUT', 'SKILL SET', 'PROJECTS','EXPERIENCE']
-    const [activeIndex, setActiveIndex] = useState(0)
-    return (
-        <Header>
-            <Header.Logo src='./../../images/icons/logo.svg'/>
-            <Header.Nav>
-                <Header.NavList>
-                    { navArray.map((navItem, index)=>{
-                       return(
-                       <Header.NavListItem key={index} onClick={()=>setActiveIndex(index)} isActive={activeIndex===index}>
-                            <Header.NavListLink href="#">{navItem}</Header.NavListLink>
-                        </Header.NavListItem>
-                    )})}
-                </Header.NavList>
-            </Header.Nav>
-            <Header.Contact>CONTACT ME</Header.Contact>
-        </Header>
-    )
+export default function HeaderContainer() {
+  const navArray = ["HOME", "ABOUT", "SKILL SET", "PROJECTS", "EXPERIENCE"];
+  const [activeIndex, setActiveIndex] = useState(0);
+  return (
+    <Header>
+      <Header.Logo src="./../../images/icons/logo.svg" />
+      <Header.Nav>
+        <Header.NavList>
+          {navArray.map((navItem, index) => {
+            return (
+              <Header.NavListItem
+                key={index}
+                onClick={() => setActiveIndex(index)}
+                isActive={activeIndex === index}
+              >
+                <Header.NavListLink href="#">{navItem}</Header.NavListLink>
+              </Header.NavListItem>
+            );
+          })}
+        </Header.NavList>
+      </Header.Nav>
+    </Header>
+  );
 }
