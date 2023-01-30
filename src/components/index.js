82ba8e85f04033bf68d4f0e9a156523cd8a82400
okaycodes <fo.okenana@gmail.com>
2023-01-30 16:11:20 +0100

    refactor: clean up unused files

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Header } from "../features/header/header";
-export { default as Home } from "./home";
-export { default as AboutMe } from "./about";
-export { default as SkillSet } from "../features/skills";
